7b12971cea888b4dd1bc94d9b5a5816fde67211a
Student <example@example.com>
2019-11-04 16:49:15 -0800

    add some functionality

--- js/scripts.js
+++ js/scripts.js
@@ -52,11 +52,27 @@ Contact.prototype.fullName = function() {
 
 
 
-//User Interfact Logic
-$(document).ready(function(){
-  $("#").submit(function(event){
+// User Interface Logic ---------
+var addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay) {
+  var contactsList = $("ul#contacts");
+  var htmlForContactInfo = "";
+  addressBookToDisplay.contacts.forEach(function(contact) {
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+};
+
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
     event.preventDefault();
-
-
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedPhoneNumber = $("input#new-phone-number").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    displayContactDetails(addressBook);
   })
-});
+})
